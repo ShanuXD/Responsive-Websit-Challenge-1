e344d1c65fef0824af5e836c3d4fc1e9d1bdb0d6
shanuXD <shanu09.sm@gmail.com>
2021-01-28 18:43:34 +0530

    overlay added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const menu_btn = document.querySelector("#menu-btn")
+const header =document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+menu_btn.addEventListener('click',()=>{
+    console.log('click')
+    
+    if(header.classList.contains('open-menu')){
+        // close menu
+        header.classList.remove('open-menu')
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else{
+        // open menu
+        header.classList.add('open-menu')
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
+
