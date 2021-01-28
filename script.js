5786d2451981f9d98fc9f4a6d7fbde8699859ded
shanuXD <shanu09.sm@gmail.com>
2021-01-28 19:39:33 +0530

    nav menu animation

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const menu_btn = document.querySelector("#menu-btn")
 const header =document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
 
 menu_btn.addEventListener('click',()=>{
     console.log('click')
     
     if(header.classList.contains('open-menu')){
         // close menu
+        body.classList.remove('noscroll')
         header.classList.remove('open-menu')
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach((element)=>{
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+        
     }
     else{
         // open menu
+        body.classList.add('noscroll')
         header.classList.add('open-menu')
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach((element)=>{
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
+
+        
     }
 })
 
